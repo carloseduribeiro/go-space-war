da3de7de82e826a1e3667a5238544b63cf7c400f
carloseduribeiro <carloseribeiro96@gmail.com>
2024-06-02 17:02:40 -0300

    Renaming window title

--- main.go
+++ main.go
@@ -1,17 +1,15 @@
 package main
 
 import (
-	"log"
-
 	"github.com/carloseduribeiro/go-space-war/internal/game"
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
 func main() {
 	ebiten.SetWindowSize(640, 480)
-	ebiten.SetWindowTitle("Hello, World!")
+	ebiten.SetWindowTitle("Go Space War")
 	g := game.NewGame()
 	if err := ebiten.RunGame(g); err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 }
